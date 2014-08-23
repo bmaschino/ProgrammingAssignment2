1025432b3dedc9fed3e1dacde0848eddb11972bd
Bryce Maschino <bmaschino@gmail.com>
2014-08-23 15:40:01 -0400

    Try #1

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setInverse <- function(solve) m <<- solve ## use the value of m from the invoking environment and calculate the inverse matrix
+     getInverse <- function() m
+     list(set = set, get = get,
+          setInverse = setInverse,
+          getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks to see if the inverse of the matrix
+## is in the list generated in makeCacheMatrix
+## If it is, it retrieves the stored variable
+## If not, it calculates and stores the variable back to the list
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     m <- x$getInverse()  ## Get the Inverse
+     if(!is.null(m)) {    ## Return inverse unless it is empty
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()    ## retrieve orignal matrix
+     m <- solve(data, ...)  ## calculate inverse matrix
+     x$setInverse(m)    ## assign inverse matrix to original variable 
+     m
 }
